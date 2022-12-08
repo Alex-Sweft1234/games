9ae6dccac482b7707e477d75944b299b4b7338e1
Александр Бабаев <babaev@atoms.ru>
2022-12-08 11:37:16 +0300

    network first sw strategy

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -12,7 +12,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -23,6 +23,12 @@ clientsClaim();
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST);
+
+// Network First (Network Falling Back to Cache)
+registerRoute(
+  ({url}) => url.pathname.startsWith('/social-timeline/'),
+  new NetworkFirst()
+);
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
